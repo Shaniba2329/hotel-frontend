cf75099d25449a399e9a521060205800f336d5ea
Shaniba2329 <shanibamariyam00@gmail.com>
2024-06-13 02:48:15 +0530

    completed

---
+++ src/components/Add.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Add = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">hotelid</label><input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">name</label><input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">category</label>
+                                <select name="category" id="" className="form-control">
+                                    <option value="AC">Ac</option>
+                                    <option value="NON AC">NON AC</option>
+                                </select>
+                            </div>
+
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">place</label> <input type="text" className="form-control" />
+                            </div>
+                           
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <button className="btn btn-success">submit</button>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Add
